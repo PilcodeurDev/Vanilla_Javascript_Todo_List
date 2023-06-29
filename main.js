e67ef0e4e1cb387bb5ca6c5a7a259616dea59344
Simon Despres simondprs62790@hotmail.fr <simondprs62790@hotmail.fr>
2023-06-29 17:06:26 +0200

    add categorys: All/Finished/Do it for put into order

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Ecouteurs
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("input", filterTodo);
 
 //Fonctions
 function addTodo(event) {
@@ -50,4 +52,29 @@ function deleteCheck(event) {
     item.parentElement.classList.toggle("completed");
   }
 
+}
+
+function filterTodo(event) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  })
 }
